41b5eb83c0ed565fcb66abc745fe55d772f85b25
Kendall Arneaud <kendall.arneaud@gmail.com>
2022-10-05 17:41:19 +0000

    Fix triple digit numbers for milliseconds

--- src/components/gameScreenComponents/Timer.tsx
+++ src/components/gameScreenComponents/Timer.tsx
@@ -13,23 +13,21 @@ const Timer = () => {
   const { gameProperties, setGameProperties } = useGameContext();
 
   const renderer = ({ minutes, seconds, milliseconds }: countdownProps) => {
-    if (seconds % 2 == 0) timeSFx.play();
+    if (seconds % 4 == 0) timeSFx.play();
     return (
       <time>
         {minutes < 10 ? "0" + minutes : minutes}:
         {seconds < 10 ? "0" + seconds : seconds}:
         {milliseconds < 100
-          ? "0" + milliseconds
-          : milliseconds < 10
-          ? "00" + milliseconds
-          : milliseconds}
+          ? "0".concat(Math.round(milliseconds / 10).toString())
+          : Math.round(milliseconds / 10)}
       </time>
     );
   };
 
   return (
     <div className="col s12">
-      <div className="clock pink z-depth-2">
+      <div className="clock pink-text">
         {" "}
         <Countdown
           date={Date.now() + 25000}
